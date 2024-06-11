e16cd1c11d960b48282efc85a98516e095a165cb
Vitiki <166802346+Vitiki@users.noreply.github.com>
2024-06-11 17:56:52 +0200

    Arreglo que los favoritos carguen bien
    Arreglo minimo, los favoritos cargan ya bien

--- AppProyecto/applicacion_tfg/lib/controllers/lista_favoritos.dart
+++ AppProyecto/applicacion_tfg/lib/controllers/lista_favoritos.dart
@@ -17,7 +17,7 @@ class _ListaFavoritosState extends State<ListaFavoritos> {
       final response = await supabase
           .from('favoritos2')
           .select(
-              'id_user, productos(correo,titulo,descripcion,productoTipo,rutaurl)')
+              'id_user, productos(id,correo,titulo,descripcion,productoTipo,rutaurl)')
           .eq('id_user', user);
 
       setState(() {
@@ -64,7 +64,7 @@ class _ListaFavoritosState extends State<ListaFavoritos> {
                   final producto = item['productos'];
                   return GestureDetector(
                     onTap: () {
-                      context.push('/pantallaProducto', extra: item);
+                      context.push('/pantallaProducto', extra: item['productos']);
                     },
                     child: Container(
                       decoration: BoxDecoration(
